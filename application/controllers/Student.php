d79ad29637bfa6279acede4e6b15970ec0bceae2
ianace <ace_escoton@yahoo.com>
2016-08-07 01:42:48 +0800

    added myProfile and myLessons

---
+++ application/controllers/Student.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Student extends CI_Controller {
+
+    public function myLessons($action = "view")
+    {
+        $this->load->view('templates/header2');
+        $this->load->view('student/myLessons');
+        $this->load->view('templates/footer');
+    }
+
+    public function myProfile($action = "view")
+    {
+        $this->load->view('templates/header2');
+        $this->load->view('student/myProfile');
+        $this->load->view('templates/footer');
+    }
+}
