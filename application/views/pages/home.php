1fd3adb1a0d41d96a2e2bf6f9723881d35d3f4c1
ianace <ace_escoton@yahoo.com>
2016-08-06 19:27:50 +0800

    added ci framework

---
+++ application/views/pages/home.php
@@ -0,0 +1,168 @@
+<nav role="navigation" class="navbar navbar-default navbar-fixed-top">
+			<div>
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+					<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+					<div class = "col-md-2"></div><div class = "col-md-4"><a href="laureleye.com" class="navbar-brand" id = "brand-text"><p class = "navtext">laureleye</p></a></div>
+					<div class = "col-md-4">
+				<!-- Collection of nav links and other content for toggling -->
+				<div id="navbarCollapse" class="collapse navbar-collapse">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="login.html" title = "Log In"><p class = "navtext">Log In</p></a></li>
+						<li><a href="signup.html" title = "Sign Up"><p class = "navtext">Sign Up</p></a></li>
+					</ul>
+				</div>
+				</div>
+			</div>	
+		</nav>
+	
+	<!-- DIV 1
+	--
+	--
+	--
+	-->
+	<div class="container-fluid bg-1">
+		<div class = "container">
+		<div id = "container banner">
+			<div>
+				<center>
+					<img src = "imgs/logo1.png"></img><br>
+					<img src = "imgs/logo2.png"></img>
+					<img src = "imgs/slogan.png"></img>
+					<div class = "container col-md-6">
+						<button type="button" class="btn btn-primary btn-block">Seek</button>
+					</div>
+					<div class = "container col-md-6">
+						<button type="button" class="btn btn-default btn-block">Teach</button>
+					</div>
+				</center>
+			</div>
+			
+		</div>
+		</div>
+	</div>
+	
+	<!-- DIV 2
+	--
+	--
+	--
+	-->
+	<div class ="container-fluid bg-2">
+	<div class="container content">
+			
+			<h1 align = "center">LaurelEye helps you learn better</h1><br><br>
+			<div class = "col-md-4 div2text">
+				<div><img src="img/rfather.png" height = "200px" width = "200px" ></div>
+				<p class = "div2text">
+					<br>Learn <i>any</i> language<br>
+					Some content here.
+				</p>
+			</div>
+			<div class = "col-md-4 div2text">
+				<div><img src="img/rmom.png" height = "200px" width = "200px" ></div>
+				<p class = "div2text">
+					<br>Learn <i>any</i> language<br>
+					Some content here.
+				</p>
+			</div>
+			<div class = "col-md-4 div2text">
+				<div><img src="img/rbro1.png" height = "200px" width = "200px" ></div>
+				<p class = "div2text">
+					<br>Learn <i>any</i> language<br>
+					Some content here.
+				</p>
+			</div>
+			<br>
+			<div class = "col-md-12"></div>
+			<div class = "col-md-12"></div>
+			<div class = "col-md-4 div2text">
+			<div><img src="img/rbro2.png" height = "200px" width = "200px" ></div>
+				<p class = "div2text">
+					<br>Learn <i>any</i> language<br>
+					Some content here.
+				</p>
+			</div>
+			<div class = "col-md-4 div2text">
+			<div><img src="img/rbro3.png" height = "200px" width = "200px" ></div>
+				<p class = "div2text">
+					<br>Learn <i>any</i> language<br>
+					Some content here.
+				</p>
+			</div>
+			<div class = "col-md-4 div2text">
+			<div><img src="img/rme.png" height = "200px" width = "200px" ></div>
+				<p class = "div2text">
+					<br>Learn <i>any</i> language<br>
+					Some content here.
+				</p>
+			</div>
+			</div>
+		</div>
+	</div>
+	</div>
+	
+	<!-- DIV 3
+	--
+	--
+	--
+	-->
+	<div class ="container-fluid bg-3">
+		<div class = "container">
+			<h1 align = "center">How It Works</h1><br><br>
+			<!--1-->
+			<div class = "container">
+				<div class = "container col-md-4">
+					<img src="img/rme.png" height = "200px" width = "200px" >
+				</div>
+				<div class = "container col-md-8">
+					<p> Some content here. </p>
+				</div>
+			</div>
+			<!--2-->
+			<div class = "container">
+				<div class = "container col-md-4">
+					<img src="img/rme.png" height = "200px" width = "200px" >
+				</div>
+				<div class = "container col-md-8">
+					<p> Some content here. </p>
+				</div>
+			</div>
+			<!--3-->
+			<div class = "container">
+				<div class = "container col-md-4">
+					<img src="img/rme.png" height = "200px" width = "200px" >
+				</div>
+				<div class = "container col-md-8">
+					<p> Some content here. </p>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class ="container-fluid bg-4">
+		<div class = "container">
+			<!-- DIV 4 -->
+			<h1 align = "center">What people are saying</h1><br><br>
+			
+		</div>
+	</div>
+	
+	<div class ="container-fluid bg-5">
+		<div class = "container">
+			<!-- DIV 5 -->
+			<h1 align = "center">Joing millions of learners</h1><br><br>
+		</div>
+	</div>
+	
+	<div class ="container-fluid bg-6">
+		<div class = "container">
+			<!-- DIV 6 -->
+			<h1 align = "center">Footer</h1><br><br>
+		</div>
+	</div>
---
+++ application/views/templates/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>LaurelEye - To Seek Knowledge</title>
+	<link rel="stylesheet" href="../../assets/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="../../assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../../assets/bootstrap/css/bootstrap-theme.min.css">
+	<script src="../../assets/js/jquery.min.js"></script>
+	<script src="../../assets/bootstrap/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="../../assets/style.css">
+	<style>
+		body{
+			padding-top: 0px;
+			background-color: black;
+			<!--background-image: url("imgs/hh2.jpg");-->
+			background-repeat:repeat;
+			background-size:cover;
+		}
+		.bg-1 { 
+			<!--background-image: url("imgs/bg2.jpg");--->
+			background-color : #d28b3f;
+			background-size: cover;
+			color: #ffffff;
+		}
+		.bg-2 { 
+			background-color: #242729;
+			color: #9BA8B1;
+			padding-bottom : 0px;
+		}
+		.bg-3 {
+			background-color: #d28b3f;
+			color: white;
+		}
+		.bg-3 {
+			background-color: #d28b3f;
+			color: white;
+		}
+		.bg-3 {
+			background-color: #d28b3f;
+			color: white;
+		}
+		.bg-4 {
+			background-color : #2429729;
+		}
+		.bg-5 {
+			background-color: #d28b3f;
+		}
+		.bg-6 {
+			background-color : #2429729;
+		}
+		.container-fluid {
+			padding-top: 70px;
+			padding-bottom: 70px;
+		}
+
+</style>
+</head> 
+	
+<body>
